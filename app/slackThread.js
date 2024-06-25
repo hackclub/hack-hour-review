08057794354864effa2326e6b83813d21f742c77
Max Wofford <max@maxwofford.com>
2024-06-25 14:18:42 -0400

    Add different views for slack message or slack file

--- app/slackThread.js
+++ app/slackThread.js
@@ -6,6 +6,29 @@ const NoSlackMessages = () => (
   <div>No Slack Messages</div>
 )
 
+const SlackMessage = ({message}) => {
+  if (!message) {
+    return null;
+  } else if (message.text == "" && message.files) {
+    // file upload
+      return (
+        <>
+          {message.files.map((file, i) => (
+            <p key={i}>
+              <em>{message.user} uploaded</em>{' '}
+              <a href="file.thumb_480" target="_blank">
+                {file.title}
+              </a>
+            </p>
+          ))}
+        </>
+      )
+  } else {
+    return <span><em>Username not yet implemented</em>: {message.text}</span>
+  }
+
+}
+
 export default function SlackThread(props) {
   const { messages } = props || [];
   const filteredMessages = messages.filter((message) => !message.bot_id)
@@ -18,9 +41,10 @@ export default function SlackThread(props) {
       <h2>Slack thread!</h2>
       <ul>
         {filteredMessages.map((msg, i) => (
-          <div key={i}>
+          <li key={i}>
+            <SlackMessage message={msg} />
             <HTMLComment text={JSON.stringify(msg, null, 2)} />
-          </div>
+          </li>
         ))}
       </ul>
     </>
