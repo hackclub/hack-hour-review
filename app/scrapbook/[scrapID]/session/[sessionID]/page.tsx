4cfdcc0bb78a97c0d9e3ed8f5551ef54813de7a4
Micha Albert <micha@2231puppy.tech>
2024-06-25 15:40:45 -0400

    changes

--- app/scrapbook/[scrapID]/session/[sessionID]/page.tsx
+++ app/scrapbook/[scrapID]/session/[sessionID]/page.tsx
@@ -7,15 +7,17 @@ import JsonMessageFormatter from "app/lib/messages";
 
 import pluralize from "../../../../lib/pluralize";
 import SlackThread from "../../../../slackThread";
+import Header from "app/lib/header";
 
 export default function Scrapbook() {
+  const [buttonStates, setButtonStates] = useState(["none", "none", "none"]);
   const [name, setName] = useState("");
   const [scrap, setScrap] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [curSession, setCurSession] = useState(0);
   const params = useParams();
-  const { scrapID } = params;
+  const { scrapID, sessionID } = params;
 
   useEffect(() => {
     const fetchData = async () => {
@@ -45,8 +47,9 @@ export default function Scrapbook() {
     }
   }, [scrap]);
 
-  const approveSession = () => {
-    // approve session, and then...
+  const approveSession = async () => {
+    setButtonStates(["otherPressed", "otherPressed", "pressed"]);
+    // const resp = await fetch(`/api/session/${sessionID}/approve`);
     if (curSession >= scrap.sessions.length - 1) {
       // next session
     } else {
@@ -77,19 +80,13 @@ export default function Scrapbook() {
   return (
     <>
       <div className="h-[15vh]">
-        <p className="text-xl text-center">
-          You're reviewing {name}'s scrapbook post!
-        </p>
-        <p className="text-center">
-          They have {pluralize("session", scrap.sessions.length, true)} to
-          review.
-        </p>
-        <p className="text-center">
-          You are reviewing session #{curSession + 1}
-        </p>{" "}
+        <Header
+          name={name}
+          length={scrap.sessions.length}
+          curSession={curSession}
+        />
+        <SlackThread messages={scrap.sessions[curSession].messages} />
       </div>
-
-      <SlackThread messages={scrap.sessions[curSession].messages} />
 
       <div className="mx-auto bg-gray-800 w-[85vw] h-[70vh] rounded-2xl">
         <JsonMessageFormatter
@@ -97,19 +94,31 @@ export default function Scrapbook() {
         ></JsonMessageFormatter>
       </div>
       <div className="w-screen h-[15vh] bottom-10 py-12 grid grid-rows-1 grid-cols-3 gap-x-4 px-4">
-        <button className="bg-orange-400 rounded-md" onClick={undoSession}>
+        <button
+          className="bg-orange-400 rounded-md disabled:bg-gray-400"
+          onClick={undoSession}
+          disabled={buttonStates[0] === "otherPressed" ? true : null}
+        >
           <p className="text-center mx-4 lg:text-lg md:text-md sm:text-sm">
-            Previous
+            {buttonStates[0] !== "pressed" ? "Previous" : "Going Back..."}
           </p>
         </button>
-        <button className="bg-red-400 rounded-md" onClick={rejectSession}>
+        <button
+          className="bg-red-400 rounded-md disabled:bg-gray-400"
+          onClick={rejectSession}
+          disabled={buttonStates[1] === "otherPressed" ? true : null}
+        >
           <p className="text-center mx-4 lg:text-lg md:text-md sm:text-sm">
-            Reject
+            {buttonStates[1] !== "pressed" ? "Reject" : "Rejecting..."}
           </p>
         </button>
-        <button className="bg-green-400 rounded-md" onClick={approveSession}>
+        <button
+          className="bg-green-400 rounded-md disabled:bg-gray-400"
+          onClick={approveSession}
+          disabled={buttonStates[2] === "otherPressed" ? true : null}
+        >
           <p className="text-center mx-4 lg:text-lg md:text-md sm:text-sm">
-            Approve
+            {buttonStates[2] !== "pressed" ? "Approve" : "Approving..."}
           </p>
         </button>
       </div>
